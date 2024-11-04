cb3d9db208967e9b110bc84ff9c7618ff7180ac8
unknown <victoralangonzalez8@gmail.com>
2024-11-04 09:38:38 -0600

    01 condicionales 1

---
+++ condicionales1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Introduce el primer numero: "))
+num2= int(input("Intrduce el segundo mumero: "))
+
+if num1 > num2:
+    print(f"el mayor es: {num1}")
+elif num1 < num2:
+    print(f"el mayor es igual: {num2}")
+else:
+    print("son iguales")        
