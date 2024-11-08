b4ed85dbe9b0e6e2be4a8f871ebbb3c2cbdabd69
unknown <victoralangonzalez8@gmail.com>
2024-11-08 12:38:31 -0600

    04 condicionales 4

---
+++ condicionales4.py
@@ -0,0 +1,8 @@
+numerador = float(input("Ingresa el numerador: "))
+denominador = float(input("Ingrese el denominador: "))
+
+if denominador != 0:
+    division = numerador / denominador
+    print(f"Ladivision es: {division}")
+else:
+    print("Error: Nose puede dividir entre cero.")
