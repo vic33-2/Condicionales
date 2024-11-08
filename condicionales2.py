60b51511405eac8dbc6ba1e2ba1716b0dd552a67
unknown <victoralangonzalez8@gmail.com>
2024-11-08 12:58:01 -0600

    05 condicionales 5

--- condicionales2.py
+++ condicionales2.py
@@ -3,7 +3,7 @@ numero = float(input("introduce un numero: "))
 
 if numero > 0:
     print("el numero es positivo")
-elif numero < 0:
+if numero < 0:
     print("el numero es negativo")  
 else:
     print("el numero es 0")    
---
+++ condicionales5.py
@@ -0,0 +1,8 @@
+usuario = input("introduce tu nombre de usuario: ")
+contrasena = input("introduce la contrasena: ")
+
+
+if usuario == "victor" and contrasena == "asdasd":
+    print("Tu entraste al sistema.")
+else:
+    print("Error: tu contrasena o usuario son incorectos.")
