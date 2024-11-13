1bd58e61983f401f73c78cd84294e26d6413c388
unknown <victoralangonzalez8@gmail.com>
2024-11-12 21:41:23 -0600

    09 condicionales 9

--- condicionales9.py
+++ condicionales9.py
@@ -1,1 +1,6 @@
-num1 = int(input("ingresa un"))
+num1 = int(input("introduce el primer numero: "))
+num2 = int(input("introduce el segundo numero: "))
+num3= int(input("introduce el tercer numero: "))
+numeros = [num1,num2,num3]
+numeros.sort(reverse=True)
+print("los numeros ordenados de mayor a menor son:", numeros)
