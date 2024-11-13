05fb37ace17c3c4bdc3d33dfbc2f29c5773c05e0
unknown <victoralangonzalez8@gmail.com>
2024-11-12 22:32:26 -0600

    13 condicionales 13

---
+++ condicionales13.py
@@ -0,0 +1,18 @@
+dia = int(input("ingresa un numero del 1 al 7: "))
+if dia == 1:
+    print("lunes")
+elif dia == 2:
+    print("martes")
+elif dia == 3:
+    print("miercoles")
+elif dia == 4:
+    print("jueves")            
+elif dia == 5:
+    print("viernes")
+elif dia == 6:
+    print("sabado")
+elif dia == 7:
+    print("domingo")    
+
+else:
+    print("Error: numero incorecto.")    
