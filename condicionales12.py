5ccca9ecd5181286494720d566f7a3a23c19d59d
unknown <victoralangonzalez8@gmail.com>
2024-11-12 22:23:22 -0600

    12 condicionales 12

---
+++ condicionales12.py
@@ -0,0 +1,15 @@
+numero_dado = int(input("igrese el numero dado al tirar el dado (1-6): "))
+if numero_dado == 1:
+    print("la cara es 4.")
+elif numero_dado == 2:
+    print("la cara es 1.")
+elif numero_dado == 3:
+    print("la cara es 3.") 
+elif numero_dado == 4:
+    print("la cara es 6.") 
+elif numero_dado == 5:
+    print("la cara es 2.")
+elif numero_dado == 6:
+     print("la cara es 5.")
+else:
+    print("Error: numero incorecto.")
